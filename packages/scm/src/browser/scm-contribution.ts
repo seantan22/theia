48eb45addda4f805cb4b310f8de930b02db2a440
seantan22 <sean.a.tan@ericsson.com>
2021-01-13 10:23:04 -0500

    Refactor SCM View Mode Type
    What It Does
    
    Creates new type for SCM view mode and refactors accordingly.
    
    Signed-off-by: seantan22 <sean.a.tan@ericsson.com>

--- packages/scm/src/browser/scm-contribution.ts
+++ packages/scm/src/browser/scm-contribution.ts
@@ -37,6 +37,7 @@ import { ScmQuickOpenService } from './scm-quick-open-service';
 import { ColorContribution } from '@theia/core/lib/browser/color-application-contribution';
 import { ColorRegistry, Color } from '@theia/core/lib/browser/color-registry';
 import { ScmCommand } from './scm-provider';
+import { ScmViewMode } from './scm-preferences';
 
 export const SCM_WIDGET_FACTORY_ID = ScmWidget.ID;
 export const SCM_VIEW_CONTAINER_ID = 'scm-view-container';
@@ -159,7 +160,7 @@ export class ScmContribution extends AbstractViewContribution<ScmWidget> impleme
                 }
             }
         };
-        const registerToggleViewItem = (command: Command, mode: 'tree' | 'list') => {
+        const registerToggleViewItem = (command: Command, mode: ScmViewMode) => {
             const id = command.id;
             const item: TabBarToolbarItem = {
                 id,
@@ -231,7 +232,7 @@ export class ScmContribution extends AbstractViewContribution<ScmWidget> impleme
         if (!repository) {
             return undefined;
         }
-        return  repository.provider.acceptInputCommand;
+        return repository.provider.acceptInputCommand;
     }
 
     protected readonly statusBarDisposable = new DisposableCollection();
