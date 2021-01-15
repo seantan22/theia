f57149867f093152ad9a7d2bbfc2fd97f77bfb07
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-01-15 11:51:14 -0500

    toggle example
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- examples/api-samples/src/browser/view/sample-unclosable-view-contribution.ts
+++ examples/api-samples/src/browser/view/sample-unclosable-view-contribution.ts
@@ -14,14 +14,33 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { AbstractViewContribution, bindViewContribution, WidgetFactory } from '@theia/core/lib/browser';
+import { AbstractViewContribution, bindViewContribution, Widget, WidgetFactory } from '@theia/core/lib/browser';
 import { SampleViewUnclosableView } from './sample-unclosable-view';
-import { injectable, interfaces } from 'inversify';
+import { inject, injectable, interfaces } from 'inversify';
+import { TabBarToolbarContribution, TabBarToolbarRegistry } from '@theia/core/lib/browser/shell/tab-bar-toolbar';
+import { Command, CommandRegistry } from '@theia/core/lib/common/command';
+import { MessageService } from 'packages/core/lib/common';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+
+export const SampleToolBarCommand: Command = {
+    id: 'navigator.toggle.sampleToolbarCommand',
+    category: 'Sample',
+    label: 'Sample Toolbar Command',
+    iconClass: 'theia-add-icon'
+};
 
 @injectable()
-export class SampleUnclosableViewContribution extends AbstractViewContribution<SampleViewUnclosableView> {
+export class SampleUnclosableViewContribution extends AbstractViewContribution<SampleViewUnclosableView> implements TabBarToolbarContribution {
+
+    protected isCommandToggled = false;
 
     static readonly SAMPLE_UNCLOSABLE_VIEW_TOGGLE_COMMAND_ID = 'sampleUnclosableView:toggle';
+
+    @inject(MessageService)
+    protected readonly messageService: MessageService;
+
+    @inject(WorkspaceService)
+    protected readonly workspaceService: WorkspaceService;
 
     constructor() {
         super({
@@ -33,10 +52,39 @@ export class SampleUnclosableViewContribution extends AbstractViewContribution<S
             }
         });
     }
+
+    protected withWidget<T>(widget: Widget | undefined = this.tryGetWidget(), cb: (sampleView: SampleViewUnclosableView) => T): T | false {
+        if (widget instanceof SampleViewUnclosableView && widget.id === SampleViewUnclosableView.ID) {
+            return cb(widget);
+        }
+        return false;
+    }
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(SampleToolBarCommand, {
+            execute: () => {
+                this.isCommandToggled = !this.isCommandToggled;
+                this.messageService.info(`${SampleToolBarCommand.label} is toggled = ${this.isCommandToggled}`);
+            },
+            isEnabled: widget => this.withWidget(widget, () => this.workspaceService.opened),
+            isVisible: widget => this.withWidget(widget, () => this.workspaceService.opened),
+            isToggled: () => this.isCommandToggled
+        });
+    }
+
+    async registerToolbarItems(toolbarRegistry: TabBarToolbarRegistry): Promise<void> {
+        toolbarRegistry.registerItem({
+            id: SampleToolBarCommand.id,
+            command: SampleToolBarCommand.id,
+            tooltip: 'Click to Toggle Toolbar Item',
+            priority: 0,
+        });
+    }
 }
 
 export const bindSampleUnclosableView = (bind: interfaces.Bind) => {
     bindViewContribution(bind, SampleUnclosableViewContribution);
+    bind(TabBarToolbarContribution).to(SampleUnclosableViewContribution).inSingletonScope();
     bind(SampleViewUnclosableView).toSelf();
     bind(WidgetFactory).toDynamicValue(ctx => ({
         id: SampleViewUnclosableView.ID,
